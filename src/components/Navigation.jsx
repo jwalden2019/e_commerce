d5b974fc29483f25c542d0b521b3cca9a444a798
James Walden <78229982+jwalden2019@users.noreply.github.com>
2021-06-09 15:28:52 -0400

    more media queries

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,13 +5,14 @@ import { Link } from "react-router-dom";
 export default function Navigation() {
     return (
         <div className="navigation__container">
-                    <nav className="navigation__links">
-                        <Link to="/LandingPage" className="navigation__name">ArtMarket</Link>
-                        <Link to="/Products" className="navigation__artworks">Artworks</Link>
-                        <Link to="/AboutUs" className="navigation__about">About Us</Link>
-                        <Link to="/Cart" className="navigation__cart">Collection</Link>
-                    </nav>
-                
-        </div>
+                    <div className="navigation__nameContainer">
+                            <Link to="/LandingPage" className="navigation__name">ArtMarket</Link>
+                        </div>
+                        <div className="navigation__linkContainer">
+                            <Link to="/Products" className="navigation__artworks">Artworks</Link>
+                            <Link to="/AboutUs" className="navigation__about">About Us</Link>
+                            <Link to="/Cart" className="navigation__cart">Collector</Link>
+                        </div>
+                    </div>
     )
 }
